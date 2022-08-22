a3cc4090e2db906e2e9bfe1fd9d8b21bc1221015
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2022-08-22 15:56:31 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd openwrt
-# Add a feed source
-echo "src-git SSRplus https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+# wireless
+rm -rf files/etc/config/wireless
+rm -rf files/etc/modules.d/wireless_enable
+# amlogic
+rm -rf files/etc/config/amlogic
+# network
+rm -rf files/etc/config/network
+# dhcp
+rm -rf files/etc/config/dhcp
