f2e0f27262a093d13481e798e675445eaf5cd9f8
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2022-12-11 16:32:17 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -5,3 +5,7 @@ echo "src-git SSRplus https://github.com/fw876/helloworld.git" >> ./feeds.conf.d
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 echo "src-git oaf https://github.com/destan19/OpenAppFilter.git" >> ./feeds.conf.default
+# Add luci-app-poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git package-temp
+mv -f package-temp/luci-app-poweroff package/lean/
+rm -rf package-tem
